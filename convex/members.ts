a99f0931ade55b976719e6110a007bd56007e2dd
Sushant Gandhi <sushant@simplified.co>
2025-02-20 22:47:34 +0530

    added change role, delete and leave functionality

--- convex/members.ts
+++ convex/members.ts
@@ -2,7 +2,7 @@ import { getAuthUserId } from "@convex-dev/auth/server";
 import { v } from "convex/values";
 
 import { Id } from "./_generated/dataModel";
-import { query, QueryCtx } from "./_generated/server";
+import { mutation, query, QueryCtx } from "./_generated/server";
 
 const getMember = async (
   ctx: QueryCtx,
@@ -110,5 +110,106 @@ export const getMemberById = query({
       ...member,
       user,
     };
+  },
+});
+
+export const updateMember = mutation({
+  args: {
+    memberId: v.id("members"),
+    role: v.union(v.literal("admin"), v.literal("member")),
+  },
+  handler: async (ctx, args) => {
+    const userId = await getAuthUserId(ctx);
+
+    if (!userId) {
+      throw new Error("Unauthorized");
+    }
+
+    const member = await ctx.db.get(args.memberId);
+
+    if (!member) {
+      throw new Error("Member not found");
+    }
+
+    const currentMember = await getMember(ctx, member.workspaceId, userId);
+
+    if (!currentMember || currentMember.role !== "admin") {
+      throw new Error("Unauthorized");
+    }
+
+    await ctx.db.patch(args.memberId, {
+      role: args.role,
+    });
+
+    return args.memberId;
+  },
+});
+
+export const removeMember = mutation({
+  args: {
+    memberId: v.id("members"),
+  },
+  handler: async (ctx, args) => {
+    const userId = await getAuthUserId(ctx);
+
+    if (!userId) {
+      throw new Error("Unauthorized");
+    }
+
+    const member = await ctx.db.get(args.memberId);
+
+    if (!member) {
+      throw new Error("Member not found");
+    }
+
+    const currentMember = await getMember(ctx, member.workspaceId, userId);
+
+    if (!currentMember) {
+      throw new Error("Unauthorized");
+    }
+
+    if (member.role === "admin") {
+      throw new Error("Admin cannot be removed");
+    }
+
+    if (currentMember._id === args.memberId && currentMember.role === "admin") {
+      throw new Error("Cannot remove self if self is an admin");
+    }
+
+    const [messages, reactions, conversations] = await Promise.all([
+      ctx.db
+        .query("messages")
+        .withIndex("by_member_id", (q) => q.eq("memberId", member._id))
+        .collect(),
+      ctx.db
+        .query("reactions")
+        .withIndex("by_member_id", (q) => q.eq("memberId", member._id))
+        .collect(),
+      ctx.db
+        .query("conversations")
+        .filter((q) =>
+          q.or(
+            q.eq(q.field("memberOneId"), member._id),
+            q.eq(q.field("memberTwoId"), member._id)
+          )
+        )
+        .collect(),
+    ]);
+
+    for (const message of messages) {
+      await ctx.db.delete(message._id);
+    }
+
+    for (const reaction of reactions) {
+      await ctx.db.delete(reaction._id);
+    }
+
+    for (const conversation of conversations) {
+      await ctx.db.delete(conversation._id);
+    }
+
+    await ctx.db.delete(args.memberId);
+
+    return args.memberId;
   },
 });
