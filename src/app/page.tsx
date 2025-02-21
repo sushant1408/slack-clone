a4dd92ba102fc6d1b593dc5ac39bbd167abad62b
Sushant Gandhi <sushant@simplified.co>
2025-02-21 10:16:19 +0530

    fixed root page UX

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 
+import { LoaderIcon } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useEffect, useMemo } from "react";
 
-import { UserButton } from "@/features/auth/components/user-button";
 import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspaces";
 import { useCreateWorkspaceModal } from "@/features/workspaces/store/use-create-workspace-modal";
 
@@ -29,8 +29,8 @@ export default function Home() {
   }, [isLoading, workspaceId, open, setOpen]);
 
   return (
-    <>
-      <UserButton />
-    </>
+    <div className="h-full flex-1 flex items-center justify-center bg-[#5C3B58]">
+      <LoaderIcon className="animate-spin text-muted-foreground !size-6" />
+    </div>
   );
 }
